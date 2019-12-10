c6fdfea1c8e1c9a9012c4de71d63fb846d1c8551
Charlie <cr646531@gmail.com>
2019-12-10 16:07:04 -0500

    added ball movement

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ var ctx = canvas.getContext('2d');
 // update the canvas every 20ms
 setInterval(draw, 20);
 
-// draw the ball
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+
+// controls how many pixels the ball will move on every update
+var dx = 2;
+var dy = -2;
+
+
 function draw() {
+	
+	// draw the ball
 	ctx.beginPath();
-	ctx.arc(50, 50, 10, 0, Math.PI*2);
+	ctx.arc(x, y, 10, 0, Math.PI*2);
 	ctx.fillStyle = "blue";
 	ctx.fill();
 	ctx.closePath();
+
+	// set the next position for the ball
+	x += dx;
+	y += dy;
 }
