191d59244f6f6d23344f6ff3a701ec061e6567fb
Charlie <cr646531@gmail.com>
2019-12-11 15:56:37 -0500

    modified ball class to clean up code

--- public/scripts/ball.js
+++ public/scripts/ball.js
@@ -1,21 +1,9 @@
-
-
 export default class Ball {
-	// radius = 10;
-	// color = "blue";
-	// sAngle = 0;
-	// eAngle = 2 * Math.PI;
-	// dx = 2;
-	// dy = -2;
-
-
 	constructor(radius = 10, color = "blue", sAngle = 0, eAngle = 2 * Math.PI, dx = 2, dy = -2) {
 		this.radius = radius;
 		this.color = color;
-
 		this.sAngle = sAngle;
 		this.eAngle = eAngle;
-
 		this.dx = dx;
 		this.dy = dy;
 	}
